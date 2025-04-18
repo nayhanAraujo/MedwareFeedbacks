a224ba22345ea20bc1c09d0e229526eeee453654
Nayhan brendow <nayhan@medware.onmicrosoft.com>
2025-04-18 16:27:15 -0300

    atualizando apppy

--- app.py
+++ app.py
@@ -941,5 +941,7 @@ def cadastrar_setor():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='192.168.10.34', port=8000, debug=True)
+        app.run(debug=True)
+
+    #app.run(host='192.168.10.34', port=8000, debug=True)
 
